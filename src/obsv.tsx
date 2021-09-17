9d6d51689e391ab5d14104a49b18765a4c02db83
A_ito <BoxPistols@users.noreply.github.com>
2021-09-18 02:05:07 +0900

    Add FadeIn Hooks Base Fix

--- src/obsv.tsx
+++ src/obsv.tsx
@@ -1,24 +1,38 @@
 import React, { useRef } from "react";
-
 import useIntersectionObserver from "./usehooks-ts";
+import { css } from "@emotion/css";
+
+export const anime = css`
+  opacity: 0;
+  transition: 1s;
+
+  min-height: 100vh;
+  display: flex;
+  border: 1px dashed #999;
+  background: #ccc;
+`;
 
 const Section = (props: { title: string }) => {
   const ref = useRef<HTMLDivElement | null>(null);
   const entry = useIntersectionObserver(ref, {});
   const isVisible = !!entry?.isIntersecting;
 
-  console.log(`Render Section ${props.title}`, { isVisible });
+  // console.log(`Render Section ${props.title}`, { isVisible });
 
   return (
     <div
+      className={anime}
       ref={ref}
       style={{
-        minHeight: "100vh",
-        display: "flex",
-        border: "1px dashed #000"
+        width: isVisible ? "100%" : 0,
+        opacity: isVisible ? 1 : 0,
+        transform: isVisible ? "translateX(0%)" : "#translateX(-150%)"
       }}
     >
-      <div style={{ margin: "auto" }}>{props.title}</div>
+      <div style={{ margin: "auto" }}>
+        {props.title}
+        {/* <p>{isVisible ? "Open" : "Clone"}</p> */}
+      </div>
     </div>
   );
 };
